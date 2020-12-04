a5703e2a10990ab9fd1e5f19213b454aa7654ac8
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-12-04 16:01:59 +0900

    カードをリスト間でもドラッグ＆ドロップで移動できる機能を実装

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -12,6 +12,7 @@
           :title="item.title"
           :cards="item.cards"
           :listIndex="index"
+          @change="movingCard"
         />
         <list-add/>
       </div>
@@ -37,5 +38,10 @@ export default {
       return this.$store.getters.totalCardCount
     }
   },
+  methods: {
+    movingCard: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    },
+  }
 }
 </script>
